a5bc3e8b07484cf520c9fee7a06da196dc94df7c
svyatoslav turets <svyatoslav.turets@gmail.com>
2024-03-14 23:38:00 -0400

    Completed Secured Vault Project

---
+++ jscript.js
@@ -0,0 +1,14 @@
+// Variable which stores the greeting "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+let greeting = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+
+// Variable that stores the first calculation in three code combination, multiplication operator is used in this calculation 
+let calculationOne = 5 * 2;
+
+// Variable that stores the second calculation in three code combination, addition operator is used in this calculation
+let calculationTwo = 15 + 25;
+
+// Variable that stores the third calculation in three code combination, division operator is used in this calculation
+let calculationThree = 78/2;
+
+// Command to display a popup dialog\alert on HTML page with greeting and three generated codes in a combination  
+alert(greeting + calculationOne + " - " + calculationTwo + " - " + calculationThree)
